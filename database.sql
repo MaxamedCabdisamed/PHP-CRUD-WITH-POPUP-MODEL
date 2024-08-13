f60743a40a4dd61c7239b26afe68e5fd491abdf1
Eng.Mohamed A.samed Mohamud. <58042514+MaxamedCabdisamed@users.noreply.github.com>
2024-08-13 20:57:47 +0300

    NEW UPLOAD
    WITH NO ANY OTHER CONFIGURATION ERROR

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL,
+    course VARCHAR(50) NOT NULL,
+    contact VARCHAR(50) NOT NULL,
+)
